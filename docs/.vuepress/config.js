c0a46fa3f28f14f55eb8dfdccb5927a4fb2df7a6
ikaruga <ikaruga777@gmail.com>
2019-11-17 16:22:00 +0900

    yarn run vuepress build docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Re:醤油の一升瓶じゃあ戦えない',
+  description: '普段の',
+  theme: 'simple',
+  themeConfig: {
+    author: 'ikaruga',
+    navbar: {
+      YOUR_SITE_NAME: 'https://ikaruga.org',            
+    }    
+  }
+}
