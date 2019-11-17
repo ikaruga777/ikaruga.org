1de9ae4f3fb518b252004f3c4cf3228f8d7355ef
ikaruga <ikaruga777@gmail.com>
2019-11-17 16:36:40 +0900

    add config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,11 @@
 module.exports = {
   title: 'Re:醤油の一升瓶じゃあ戦えない',
   description: '普段の',
+  dest: path.resolve(__dirname, '../../dist'),
+  evergreen: true,
+  locales: {
+    '/': { lang: 'ja' },
+  },
   theme: 'simple',
   themeConfig: {
     author: 'ikaruga',
