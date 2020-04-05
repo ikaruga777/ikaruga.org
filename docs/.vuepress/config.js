52467f54c68fe985485742735b331c2ab2a41c90
ikaruga <ikaruga777@gmail.com>
2020-04-05 21:52:06 +0900

    frontmatterにimageいなかったら、iconだすようにする

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,6 +11,7 @@ module.exports = {
   theme: 'simple',
   themeConfig: {
     author: 'ikaruga',
+    domain: 'ikaruga.org',
     navbar: {
       "GitHub": 'https://github.com/ikaruga777',
       "Twitter": "https://twitter.com/UVB_76",
@@ -39,7 +40,7 @@ module.exports = {
         twitterCard: _ => 'summary',
         type: $page => ['articles', 'posts', 'blog'].some(folder => $page.regularPath.startsWith('/' + folder)) ? 'article' : 'website',
         url: (_, $site, path) => ($site.themeConfig.domain || '') + path,
-        image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image),
+        image: ($page, $site) => ($page.frontmatter.image && (($site.themeConfig.domain || '') + $page.frontmatter.image)) || ($site.themeConfig.domain || '') + "/icon.jpg",
         publishedAt: $page => $page.frontmatter.date && new Date($page.frontmatter.date),
         modifiedAt: $page => $page.lastUpdated && new Date($page.lastUpdated),
       }
