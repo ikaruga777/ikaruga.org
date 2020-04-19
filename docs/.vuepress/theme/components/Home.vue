190ec1c83f266b0e8e7ff9b208ff22b201865b98
ikaruga <ikaruga777@gmail.com>
2020-04-19 17:00:49 +0900

    themeをコピってくる

---
+++ docs/.vuepress/theme/components/Home.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="list-view">
+    <div
+      v-if="filteredList.length === 0"
+      class="empty-list"
+    >
+      Ooops! Nothing here..🙈
+    </div>
+    <ol
+      v-else
+      class="list">
+      <li v-for="page of filteredList"
+        :key="page.key"
+        class="list-item"
+      >
+        <router-link
+          :to="page.path"
+          class="item-title"
+        >
+          {{ page.title }}
+        </router-link>
+        <br>
+        <time-ago
+          :last-updated="page.frontmatter.date || page.lastUpdated"
+          class="item-date"
+        />
+      </li>
+    </ol>
+  <router-link v-if=hasPrev :to=prevLink>prev</router-link>
+
+  <router-link v-if=hasNext :to=nextLink>next</router-link>
+  </div>
+</template>
+
+<script>
+import TimeAgo from './TimeAgo';
+
+export default {
+  components: {
+    TimeAgo
+  },
+  computed: {
+    filteredList() {
+      // Order by publish date, desc
+      if (this.$pagination === null) {
+        return []
+      }
+      return this.$pagination.pages
+        .sort((a, b) => {
+          return new Date(b.frontmatter.date || b.lastUpdated) - new Date(a.frontmatter.date || a.lastUpdated)
+        })
+    },
+    nextLink() {
+      return this.$pagination.nextLink
+    },
+    hasNext() {
+      return this.$pagination.hasNext
+    },
+    prevLink() {
+      return this.$pagination.prevLink
+    },
+    hasPrev() {
+      return this.$pagination.hasPrev
+    }
+  }
+}
+</script>
---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="theme-container vuepress-theme-simple">
+    <header class="header">
+      <router-link
+        to="/"
+        :title="$description"
+        class="site-name"
+      >
+        {{ $site.title }}
+      </router-link>
+      <div style="clear: both" />
+      <nav-bar />
+    </header>
+    <home-page />
+    <footer-bar />
+  </div>
+</template>
+
+<script>
+import HomePage from "../components/Home";
+import FooterBar from '../components/FooterBar';
+import NavBar from '../components/NavBar';
+
+export default {
+  components: {
+    HomePage,
+    FooterBar,
+    NavBar,
+  },
+  computed: {
+    isHome() {
+      return this.$page.path === '/';
+    }
+  }
+};
+</script>
