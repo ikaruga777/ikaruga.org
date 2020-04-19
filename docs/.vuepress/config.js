644090d5d93cf55fe9fc68ea3b7a8b7ef99963a7
ikaruga <ikaruga777@gmail.com>
2020-04-19 17:01:20 +0900

    themeをpackage.jsonから読み込まないようにして、ページネーションの設定を入れる

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
   head: [
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap' }]
   ],
-  theme: 'simple',
+
   themeConfig: {
     author: 'ikaruga',
     domain: 'https://ikaruga.org',
@@ -22,6 +22,21 @@ module.exports = {
     }
   },
   plugins: [
+    [
+      '@vuepress/blog',
+      {
+        directories: [
+          {
+            id: 'post',
+            dirname: '_posts',
+            path: '/',
+            pagination: {
+              lengthPerPage: 3,
+            },
+          }
+        ]
+      }
+    ],
     [
       '@vuepress/google-analytics',
       {
