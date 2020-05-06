e7a46bfac0c8f9d07ce13b9efce93cfdedcd5d18
ikaruga <ikaruga777@gmail.com>
2020-05-06 16:57:52 +0900

    GlobalLayoutで共通ヘッダーつけたので、NotFoundのヘッダーを消す

--- docs/.vuepress/theme/layouts/404.vue
+++ docs/.vuepress/theme/layouts/404.vue
@@ -1,23 +1,10 @@
 <template>
-  <div class="theme-container vuepress-theme-simple">
-    <header class="header">
-      <router-link
-        to="/"
-        :title="$description"
-        class="site-name"
-      >
-        {{ $site.title }}
-      </router-link>
-      <div style="clear: both" />
-    </header>
-    <div class="layout-404">
-      <h1>404</h1>
-      <p>Seems nothing here.</p>
-      <router-link to="/">
-        Back to home.
-      </router-link>
-    </div>
-    <footer-bar />
+  <div class="layout-404">
+    <h1>404</h1>
+    <p>Seems nothing here.</p>
+    <router-link to="/">
+      Back to home.
+    </router-link>
   </div>
 </template>
 
