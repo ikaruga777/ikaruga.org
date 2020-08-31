26c5c1b0fdf55d6feb82600a46d761cc9abd4f0a
ikaruga <ikaruga777@gmail.com>
2020-08-31 21:33:20 +0900

    wip

--- docs/.vuepress/theme/layouts/Post.vue
+++ docs/.vuepress/theme/layouts/Post.vue
@@ -15,14 +15,11 @@
       <Vssue :title="$title" />
     </div>
     <div>
-       <router-link v-if=hasPrev :to=prevPost.path>{{ prevPost.title }}</router-link> |
-        <router-link v-if=hasPrev :to=nextPost.path> {{ nextPost.title }}</router-link>
-        {{prevPost.title}} -
-        {{currentPost.title}} -
-        {{nextPost.title}}
+       <router-link v-if="hasPrev" :to=prevPost.path>{{ prevPost.title }}</router-link> |
+       <router-link v-if="hasNext" :to=nextPost.path> {{ nextPost.title }}</router-link>
     </div>
     <div v-for="(post ,index)  in this.posts()">
-      {{index }} - {{ post.title }}
+      {{ index }} - {{ post.title }}
     </div>
   </section>
 </template>
@@ -31,26 +28,26 @@
 import TimeAgo from '../components/TimeAgo';
 
 export default {
+  name: "post",
   components: {
     TimeAgo
   },
   computed: {
     hasPrev() {
+      console.log('hasprev:' , this.posts().findIndex(post => post.key === this.$page.key) > 0)
       return this.posts().findIndex(post => post.key === this.$page.key) > 0
     },
     hasNext() {
+      console.log('hasnext:' , this.posts().findIndex(post => post.key === this.$page.key) < this.posts().length - 1)
+
       return this.posts().findIndex(post => post.key === this.$page.key) < this.posts().length - 1
     },
     prevPost() {
-      // console.log(this.posts())
-      // console.log(this.posts().findIndex(post => post.key === this.$page.key))
       console.log("prev",this.posts()[this.posts().findIndex(post => post.key === this.$page.key)-1])
 
       return this.posts()[this.posts().findIndex(post => post.key === this.$page.key)-1]
     },
     nextPost() {
-//       console.log(this.posts())
-//       console.log(this.posts().findIndex(post => post.key === this.$page.key))
       console.log("next",this.posts()[this.posts().findIndex(post => post.key === this.$page.key)+1])
 
       return this.posts()[this.posts().findIndex(post => post.key === this.$page.key)+1]
@@ -71,22 +68,39 @@ export default {
   methods: {
     posts() {
       const postReg = /^_posts\//
-      return this.$site.pages.filter( (page) => {
+      const posts = this.$site.pages.filter( (page) => {
         return /^_posts/.test(page.relativePath)
       })
+
+      const sortedPosts = posts.sort( (a,b) => {
+        const dateA = a.frontmatter.date;
+        const dateB = b.frontmatter.date;
+        if (dateA < dateB) {
+          return 1;
+        }
+        if (dateA > dateB) {
+          return -1;
+        }
+        return 0;
+      })
+      return sortedPosts;
     },
     transitionPage(e){
       if (e.code === "ArrowRight") {
-        this.transitionNextPost()
+        if (this.hasNext) {
+          this.transitionNextPost()
+        }
       }else if(e.code === "ArrowLeft") {
-        this.transitionPrevPost()
+        if (this.hasPrev) {
+          this.transitionPrevPost()
+        }
       }
     },
     transitionNextPost() {
-      this.$router.push( {path: this.nextPost.link })
+      this.$router.push( {path: this.nextPost.path })
     },
     transitionPrevPost() {
-      this.$router.push( {path: this.prevPost.link }).catch(err => {})
+      this.$router.push( {path: this.prevPost.path }).catch(err => {})
     }
   }
 
