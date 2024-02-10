dddb64d9c47d437325e2552f9066378535f440ef
ikaruga <ikaruga777@gmail.com>
2024-02-10 17:36:49 +0900

    konnnann

--- docs/.vuepress/theme/layouts/Post.vue
+++ docs/.vuepress/theme/layouts/Post.vue
@@ -30,21 +30,22 @@ export default {
   },
   computed: {
     url() {
-      return this.$themeConfig.domain + this.$page.path;
-    }
+      return this.$themeConfig.domain + this.$page.path
+    },
   },
-  mounted() {
-    const script = document.createElement('script');
-    script.src = 'https://s.hatena.ne.jp/js/HatenaStar.js';
-    document.body.appendChild(script);
+  mounted: function(){
+    const script = document.createElement('script')
+    script.src = 'https://s.hatena.ne.jp/js/HatenaStar.js'
+    document.body.appendChild(script)
     Hatena.Star.SiteConfig = {
-        entryNodes: {
-          'section': {
-            uri: 'post-title a',
-            title: 'post-title',
-            container: 'hatena-star'
-          }
+      entryNodes: {
+        'section': {
+          uri: 'post-title a',
+          title: 'post-title',
+          container: 'hatena-star'
         }
-      };
-}
+      }
+    };
+  },
+};
 </script>
